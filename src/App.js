8be6957f6da2cd034dde58588045e178ecd72076
josh.brown <josh.brown@COMP10583.local>
2019-08-07 17:43:32 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Josh made a change.
+          Edit <code>src/App.js</code> and save to reload. Josh made a change. Again.. new change!!
         </p>
         <a
           className="App-link"
